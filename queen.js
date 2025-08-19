c8cfcae7ac5468e73486d8d1e14b097c0d0e180c
shan148 <shanb5845@gmail.com>
2025-08-19 11:29:05 +0530

    Update queen.js

--- queen.js
+++ queen.js
@@ -23,7 +23,7 @@ const ownerNumber = ['94717775628']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+if(!config.SESSION_ID) return console.log('ANJU-MD~&SlomlLSZ#StrPdkakmGmRe5Yrn1arYHHTXvP5lEHbmd2djVqiWLA')
 const sessdata = config.SESSION_ID
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
